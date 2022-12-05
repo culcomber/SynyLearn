fe98b4a20e93d48b8aa686bfef3952b8e40a59fb
culcomber <m13076185140@gmail.com>
2022-12-05 20:00:00 +0800

    syny

--- code/YouDontKnowJs/get-started/GetStarted.js
+++ code/YouDontKnowJs/get-started/GetStarted.js
@@ -1,34 +1,35 @@
-function getFoo () {
-    return new Promise(function (resolve, reject){
-        resolve('foo');
-    });
+// 下面是二叉树的构造函数，三个参数分别是左树、当前节点和右树
+function Tree(left, label, right) {
+    this.left = left;
+    this.label = label;
+    this.right = right;
 }
 
-const g = function* () {
-    try {
-        const foo = yield getFoo();
-        console.log(foo);
-    } catch (e) {
-        console.log(e);
+// 下面生成二叉树
+function make(array) {
+    // 判断是否为叶节点
+    if (array.length == 1) return new Tree(null, array[0], null);
+    return new Tree(make(array[0]), array[1], make(array[2]));
+}
+let tree = make([[['a'], 'b', ['c']], 'd', [['e'], 'f', ['g']]]);
+//       d
+//   b       f
+// a   c    e  g
+
+// 下面是中序（inorder）遍历函数。
+// 由于返回的是一个遍历器，所以要用generator函数。函数体内采用递归算法，所以左树和右树要用yield*遍历
+function* inorder(t) {
+    if (t) {
+        yield* inorder(t.left);
+        yield t.label;
+        yield* inorder(t.right);
     }
-};
-
-function run (generator) {
-    const it = generator(); // 得到g迭代器
-
-    function go(result) { // 参数是it
-        // { value: Promise { 'foo' }, done: false }
-        // { value: undefined, done: true }
-        if (result.done) return result.value;
-
-        return result.value.then(function (value) { // g还有yield就继续调用
-            return go(it.next(value));
-        }, function (error) {
-            return go(it.throw(error));
-        });
-    }
-
-    go(it.next()); // 运行g
 }
 
-run(g);
+// 遍历二叉树
+var result = [];
+for (let node of inorder(tree)) {
+    result.push(node);
+}
+
+result // ['a', 'b', 'c', 'd', 'e', 'f', 'g']
