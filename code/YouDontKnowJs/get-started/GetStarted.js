861628c492ac23b3f1f0f3e93aeecea90b7fe24c
culcomber <m13076185140@gmail.com>
2022-12-08 20:59:30 +0800

    syny

--- code/YouDontKnowJs/get-started/GetStarted.js
+++ code/YouDontKnowJs/get-started/GetStarted.js
@@ -1,23 +1,21 @@
-const co = require('co');
-const fs = require('fs');
-
-const stream = fs.createReadStream('./les_miserables.txt');
-let valjeanCount = 0;
-
-co(function*() {
-    while(true) {
-        const res = yield Promise.race([
-            new Promise(resolve => stream.once('data', resolve)),
-            new Promise(resolve => stream.once('end', resolve)),
-            new Promise((resolve, reject) => stream.once('error', reject))
-        ]);
-        if (!res) {
-            break;
-        }
-        stream.removeAllListeners('data');
-        stream.removeAllListeners('end');
-        stream.removeAllListeners('error');
-        valjeanCount += (res.toString().match(/valjean/ig) || []).length;
+async function takeAsync(asyncIterable, count = Infinity) {
+    const result = [];
+    const iterator = asyncIterable[Symbol.asyncIterator]();
+    while (result.length < count) { // 遍历gen
+        const {value, done} = await iterator.next();
+        if (done) break;
+        result.push(value);
     }
-    console.log('count:', valjeanCount); // count: 1120
-});
+    return result; // ['a', 'b', 'c']
+}
+async function f() {
+    async function* gen() {
+        yield 'a';
+        yield 'b';
+        yield 'c';
+    }
+    return await takeAsync(gen());
+}
+f().then(function (result) {
+    console.log(result); // ['a', 'b', 'c']
+})
