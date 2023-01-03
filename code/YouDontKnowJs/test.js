4f470ddf957b948df6352cfb33363012b53c62f5
culcomber <m13076185140@gmail.com>
2023-01-03 19:55:37 +0800

    syny

--- code/YouDontKnowJs/test.js
+++ code/YouDontKnowJs/test.js
@@ -1,7 +1,32 @@
-let config = {
-    alert: setInterval(() => {
-        console.log('Alert!')
-    }, 1000)
+function tco(f) {
+    var value;
+    var active = false;
+    var accumulated = [];
+
+    return function accumulator() {
+        accumulated.push(arguments);
+        console.log(arguments)
+        if (!active) {
+            active = true;
+            console.log(active)
+            while (accumulated.length) {
+                value = f.apply(this, accumulated.shift());
+            }
+            active = false;
+            console.log(active)
+            return value; // return sum(x + 1, y - 1) --> accumulator(x + 1, y - 1)
+        }
+    };
 }
 
-config = null
+var sum = tco(function(x, y) {
+    if (y > 0) {
+        return sum(x + 1, y - 1)
+    }
+    else {
+        return x
+    }
+});
+
+const hello = sum(1, 3) // 100001
+console.log(hello)
