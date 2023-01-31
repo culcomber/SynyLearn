c03a3859d82c68e6bd1786ebd808961a4d75e3e0
culcomber <m13076185140@gmail.com>
2023-01-31 20:01:39 +0800

    sync

---
+++ code/50/day1/test.js
@@ -0,0 +1,14 @@
+let msg = "Hello world";
+console.log(msg);
+let numA = 6;
+let numB = 13;
+let num = numA + numB;
+console.log(num);
+
+function Hello() {
+  console.log("Hello world1");
+  console.log("Hello world3");
+  console.log("Hello world3");
+  console.log("Hello world4");
+  console.log("Hello world");zo
+}
