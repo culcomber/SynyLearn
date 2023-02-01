2ce5faddb9fc47529f7ffcad5943f2534673796f
culcomber <m13076185140@gmail.com>
2023-02-01 17:40:26 +0800

    syny

--- code/50/day1/test.js
+++ code/50/day1/test.js
@@ -1,14 +1,13 @@
 let msg = "Hello world";
-console.log(msg);
+
 let numA = 6;
 let numB = 13;
-let num = numA + numB;
 console.log(num);
+
+let num = numA + numB;
 
 function Hello() {
   console.log("Hello world1");
   console.log("Hello world3");
-  console.log("Hello world3");
   console.log("Hello world4");
-  console.log("Hello world");zo
 }
