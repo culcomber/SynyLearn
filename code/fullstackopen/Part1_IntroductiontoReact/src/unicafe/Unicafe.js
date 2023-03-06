2096350f5fd5ab8f626c7086a845b5f383e96d57
culcomber <m13076185140@gmail.com>
2023-03-06 16:23:50 +0800

    sync

--- code/fullstackopen/Part1_IntroductiontoReact/src/unicafe/Unicafe.js
+++ code/fullstackopen/Part1_IntroductiontoReact/src/unicafe/Unicafe.js
@@ -1,5 +1,44 @@
 import {useState} from "react";
 
+/*函数组件简略写法*/
+const Header = props => <h1>{props.name}</h1>
+
+/*解构赋值参数*/
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+/*条件渲染*/
+const Statistics = ({good, neutral, bad}) => {
+    const total = good + neutral + bad;
+    const average = (good - bad) / total
+    const positive = good * (100/total)
+
+    return(<>
+        {
+            total === 0
+                ? <div>
+                    No feedback given
+                </div>
+                : <div>
+                    <table>
+                        <tbody>
+                        <StatisticLine text="good" value ={good} />
+                        <StatisticLine text="neutral" value ={neutral} />
+                        <StatisticLine text="bad" value ={bad} />
+                        <StatisticLine text="all" value={total} />
+                        <StatisticLine text="average" value={average.toFixed(2)} />
+                        <StatisticLine text="positive" value={positive.toFixed(2)} />
+                        </tbody>
+                    </table>
+                </div>
+        }
+    </>)
+}
+
+
 const Unicafe = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
@@ -14,6 +53,7 @@ const Unicafe = () => {
         setBad(bad + 1);
     };
 
+    /*可以整合数据*/
     /*const [clicks, setClicks] = useState({
         good: 0, neutral: 0, bad: 0
     })
@@ -27,48 +67,9 @@ const Unicafe = () => {
     const handleBadClick = () =>
         setClicks({...clicks, bad: clicks.bad + 1})*/
 
-    const Header = props => <h1>{props.name}</h1>
-
-    const Button = ({ onClick, text }) => (
-        <button onClick={onClick}>
-            {text}
-        </button>
-    )
-
-    const Statistics = ({good, neutral, bad}) => {
-        const total = good + neutral + bad;
-        const average = (good - bad) / total
-        const positive = good * (100/total)
-
-        return(<>
-            {
-                total === 0
-                    ? <div>
-                        No feedback given
-                    </div>
-                    : <div>
-                        <table>
-                            <tbody>
-                                <StatisticLine text="good" value ={good} />
-                                <StatisticLine text="neutral" value ={neutral} />
-                                <StatisticLine text="bad" value ={bad} />
-                                <StatisticLine text="all" value={total} />
-                                <StatisticLine text="average" value={average} />
-                                <StatisticLine text="positive" value={positive} />
-                            </tbody>
-                        </table>
-                    </div>
-            }
-        </>)
+    const StatisticLine = ({ value, text }) => {
+        return text === "positive" ? <tr><td>{text}</td><td>{value}%</td></tr> : <tr><td>{text}</td><td>{value}</td></tr>;
     }
-
-    const StatisticLine = ({ value, text }) => (
-        <p>
-            {text} {value}
-        </p>
-    )
-
-
 
     return (
         <div>
