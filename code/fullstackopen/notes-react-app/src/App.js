71898d8bbc583114b6b8eb11beecd48e93bab92d
culcomber <m13076185140@gmail.com>
2023-03-09 11:26:52 +0800

    sync

--- code/fullstackopen/notes-react-app/src/App.js
+++ code/fullstackopen/notes-react-app/src/App.js
@@ -1,22 +1,22 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Note from "./components/Note";
-import noteService from './services/notes'
+import Notification from "./components/Notification";
+import noteService from "./services/notes";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     /*axios.get("http://localhost:3001/notes").then((response) => {
       setNotes(response.data);
     });*/
-    noteService
-    .getAll()
-    .then(response => {
-      setNotes(response.data)
-    })
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
   }, []);
 
   const addNote = (event) => {
@@ -28,17 +28,15 @@ const App = () => {
 
     // 在POST请求中发送的数据是一个JavaScript对象，axios自动知道为Content-Type头设置适当的application/json值
     /*axios
-      .post("http://localhost:3001/notes", noteObject) // 把数据传到后台
-      .then((response) => {
-        setNotes(notes.concat(response.data)); // 使用后台返回数据
-        setNewNote("");
-      });*/
-      noteService
-          .create(noteObject)
-          .then(response => {
-              setNotes(notes.concat(response.data))
-              setNewNote('')
-          })
+    .post("http://localhost:3001/notes", noteObject) // 把数据传到后台
+    .then((response) => {
+      setNotes(notes.concat(response.data)); // 使用后台返回数据
+      setNewNote("");
+    });*/
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -48,7 +46,6 @@ const App = () => {
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
@@ -56,15 +53,25 @@ const App = () => {
       setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
     });*/
     noteService
-        .update(id, changedNote)
-        .then(response => {
-          setNotes(notes.map(note => note.id !== id ? note : response.data))
-        })
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -85,6 +92,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
--- note/项目/fullstackopen.md
+++ note/项目/fullstackopen.md
@@ -273,13 +273,30 @@ todo [JavaScript中的函数式编程](https://www.youtube.com/playlist?list=PL0
 
    [单一责任原则](https://en.wikipedia.org/wiki/Single_responsibility_principle)，将这种通信提取到[模块](https://fullstackopen.com/en/part2/rendering_a_collection_modules#refactoring-modules)
 
+   ```js
+   // src/services/notes.js
+   import axios from 'axios'
+   const baseUrl = 'http://localhost:3001/notes'
    
-
-4. Cleaner Syntax for Defining Object Literals
-
+   const getAll = () => {
+     const request = axios.get(baseUrl)
+     return request.then(response => response.data)
+   }
    
+   const create = newObject => {
+     const request = axios.post(baseUrl, newObject)
+     return request.then(response => response.data)
+   }
+   
+   const update = (id, newObject) => {
+     const request = axios.put(`${baseUrl}/${id}`, newObject)
+     return request.then(response => response.data)
+   }
+   
+   export default { getAll, create, update }
+   ```
 
-5. Promises and Errors
+4. Promises and Errors
 
    
 
