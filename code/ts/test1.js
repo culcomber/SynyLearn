74a7d390ae1b7ba9afaac383ac23028552aded1f
SamTL <m13076185140@gmail.com>
2023-05-17 17:32:53 +0800

    hook1 2

--- code/ts/test1.js
+++ code/ts/test1.js
@@ -1,2 +1,13 @@
-let {a, b} = {a: 'q'}
-console.log(a, b);
+function Rabbit(name) {
+    this.name = name;
+}
+Rabbit.prototype.jumps = {
+    jumps: true
+};
+let rabbit = new Rabbit("White Rabbit");
+console.log(rabbit.__proto__)
+console.log(Rabbit.__proto__)
+console.log(Rabbit.__proto__.__proto__)
+console.log(Rabbit.prototype.__proto__)
+console.log('Object.prototype', Object.prototype)
+console.log('Object.prototype.__proto__', Object.prototype.__proto__)
