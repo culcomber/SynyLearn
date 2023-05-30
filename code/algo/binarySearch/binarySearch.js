a842f4b3012506f502aa207ec149adf0f16e99e0
SamTL <m13076185140@gmail.com>
2023-05-30 20:54:17 +0800

    hook1 2

--- code/algo/binarySearch/binarySearch.js
+++ code/algo/binarySearch/binarySearch.js
@@ -19,3 +19,54 @@ const search = function(nums, target) {
 };
 
 console.log(search([1, 2, 3, 4, 5, 6], 3));
+
+// Order-agnostic Binary Search (easy)
+function binary_search(arr, key) {
+    let start = 0, end = arr.length - 1, mid;
+    let isAscending = arr[start] < arr[end];
+    while (start <= end) {
+        mid = Math.floor((end - start) / 2 + start);
+        if (arr[mid] === key) {
+            return mid;
+        }
+        if (isAscending) {
+            if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        } else {
+            if (arr[mid] > key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+console.log(binary_search([4, 6, 10], 10));
+console.log(binary_search([1, 2, 3, 4, 5, 6, 7], 5));
+console.log(binary_search([10, 6, 4], 10));
+console.log(binary_search([10, 6, 4], 4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
