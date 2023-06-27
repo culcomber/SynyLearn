0ba04af385271d8b1b16a9b0f997d0c2f8e8f734
SamTL <m13076185140@gmail.com>
2023-06-27 19:47:22 +0800

    sync

---
+++ code/react/react-start-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href={`/managing-state`}>ManagingState</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default App;
