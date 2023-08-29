7cd555f7993d8ba6a92af17699b1efcc65fd4d25
SamTL <m13076185140@gmail.com>
2023-08-29 21:34:44 +0800

    Observer Pattern

---
+++ code/algo/TwoPointers/TwoPointers.js
@@ -0,0 +1,13 @@
+// 167. 两数之和 II - 输入有序数组 Medium
+/*Example 1:
+Input: [1, 2, 3, 4, 6], target=6
+Output: [1, 3]
+Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+
+Example 2:
+Input: [2, 5, 9, 11], target=11
+Output: [0, 2]
+Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11*/
+function pair_with_target_sum(arr, targetSum) {
+
+ };
