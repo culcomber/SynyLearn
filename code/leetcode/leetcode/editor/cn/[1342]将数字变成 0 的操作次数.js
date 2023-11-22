0bbb60fb9e62d0bdcc7caeea03cc9be472a5f11d
culcomber <m13076185140@gmail.com>
2023-11-22 22:59:40 +0800

    sync

--- code/leetcode/leetcode/editor/cn/[1342]将数字变成 0 的操作次数.js
+++
@@ -1,9 +0,0 @@
-//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * @param {number} num
- * @return {number}
- */
-var numberOfSteps = function(num) {
-
-};
-//leetcode submit region end(Prohibit modification and deletion)
