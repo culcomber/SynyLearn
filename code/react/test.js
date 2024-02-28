2c5e0b8896f238e22d0c2e24749ec961cca1dfec
SamTL <m13076185140@gmail.com>
2024-02-28 17:27:09 +0800

    sync

--- code/react/test.js
+++ code/react/test.js
@@ -1,6 +1,10 @@
-function foo() {
-  console.log(a);
-  var a = 1;
-}
+let o1 = { a: 1 };
+let o2 = { b: 2 };
+o2.__proto__ = o1;
+let { ...o3 } = o2;
+o3 // { b: 2 }
+o3.a // undefined
 
-foo(); // 报错，没有用var声明a
+let { o4 } = {o2};
+o4 // { b: 2 }
+o4.a // undefined
