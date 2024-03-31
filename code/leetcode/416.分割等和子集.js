3a22bd3571e69d95f7c6149ef06f9c296dfff9cb
culcomber <m13076185140@gmail.com>
2024-03-31 17:09:00 +0800

    sync

---
+++ code/leetcode/416.分割等和子集.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=416 lang=javascript
+ *
+ * [416] 分割等和子集
+ * 同 698.划分为k个相等的子集 473.火柴拼正方形
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+
+};
+// @lc code=end
+
