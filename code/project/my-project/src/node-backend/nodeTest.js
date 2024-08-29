3b93143d1958ce53a1d5aeaa1eab832b03a0a254
culcomber <m13076185140@gmail.com>
2024-08-29 18:27:12 +0800

    Changes

--- code/project/my-project/src/node-backend/nodeTest.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-import exportHello from "../just-js/exportHello.js";
-
-console.log(exportHello);
-console.log(process.env.MY_SECRET);
--- code/project/my-project/src/react-frontend/testJSX.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const testJSX = <div>testJSX</div>;
-exports.default = testJSX;
