345f2989eb576ef409835c2b15fd7953591ef23e
culcomber <m13076185140@gmail.com>
2024-09-02 16:45:14 +0800

    button样式补充

--- code/project/componentLibrary/vikingship-sam/src/App.tsx
+++ code/project/componentLibrary/vikingship-sam/src/App.tsx
@@ -6,10 +6,11 @@ function App() {
     <div>
         <div>
             <h3>Button</h3>
-            <button>Button</button>
+            <button>原生</button>
             <Button>HELLO</Button>
             <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Primary_lg</Button>
-            <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled={true}>Primary_lg</Button>
+            <Button btnType={ButtonType.Primary} size={ButtonSize.Small} disabled={true}>Primary_sm</Button>
+            <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>Danger_lg</Button>
             <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger_sm</Button>
             <Button btnType={ButtonType.Link} href="https://react.dev/">Link</Button>
             <Button btnType={ButtonType.Link} href="https://react.dev/" disabled={true}>Link disabled</Button>
